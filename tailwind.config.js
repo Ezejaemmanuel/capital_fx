2366f7b6e83363a6114fc2e932583e358d0cd9f4
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-07-07 06:57:45 -0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,60 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite-react/**/*.js",
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+      colors: {
+        primary: {
+          light: '#6ee7b7',
+          DEFAULT: '#3b82f6',
+          dark: '#9333ea',
+        },
+        secondary: {
+          light: '#fbcfe8',
+          DEFAULT: '#f87171',
+          dark: '#ef4444',
+        },
+        background: {
+          light: '#f3f4f6',
+          DEFAULT: '#ffffff',
+          dark: '#1f2937',
+        },
+        text: {
+          light: '#111827',
+          DEFAULT: '#4b5563',
+          dark: '#9ca3af',
+        },
       },
     },
   },
-  plugins: [],
+  
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
+
+  ],
 }

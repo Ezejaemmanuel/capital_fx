90726e1195ad0cc74f5599d861146425f1873ec5
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-07-09 13:34:39 -0700

    Update Header_AI.tsx

--- components/Header_AI.tsx
+++ components/Header_AI.tsx
@@ -32,8 +32,8 @@ const HeroSection: React.FC = () => {
 
     return (
         <section className="bg-white dark:bg-gray-900">
-            <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-                <div className="mr-auto place-self-center lg:col-span-7">
+            <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-8 xl:gap-0 lg:py-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-12">
+                <div className="mr-auto place-self-center sm:col-span-1 md:col-span-6 lg:col-span-7">
                     <AnimatePresence mode='wait'>
                         <motion.h1
                             className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none font-serif md:text-5xl xl:text-6xl dark:text-white  bg-clip-text text-transparent"
@@ -67,7 +67,7 @@ const HeroSection: React.FC = () => {
                         <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </Link>
                 </div>
-                <div className="mt-8 lg:mt-0 lg:col-span-5 flex justify-center items-center">
+                <div className="mt-8 lg:mt-0 flex justify-center items-center sm:col-span-1 md:col-span-6 lg:col-span-5">
                     <motion.div
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
@@ -83,5 +83,4 @@ const HeroSection: React.FC = () => {
         </section>
     );
 }
-
 export default HeroSection;

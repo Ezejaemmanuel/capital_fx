83157ef38e6040119da6e1ea489e3c9fe00be1f7
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-07-12 08:20:10 -0700

    Update card-wrapper.tsx

--- app/card-wrapper.tsx
+++ app/card-wrapper.tsx
@@ -1,12 +1,16 @@
 'use client';
-import React, { memo, useEffect } from 'react';
+import { useEffect, FC } from 'react';
 import { AnimatePresence, motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
-const FadeInWhenVisible: React.FC<{children: React.ReactNode}> = memo(({ children }) => {
+interface FadeInWhenVisibleProps {
+  children: React.ReactNode;
+}
+
+const FadeInWhenVisible: FC<FadeInWhenVisibleProps> = ({ children }) => {
   const controls = useAnimation();
   const { ref, inView } = useInView({
-    triggerOnce: false,
+    triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
   });
 
   useEffect(() => {
@@ -21,16 +25,15 @@ const FadeInWhenVisible: React.FC<{children: React.ReactNode}> = memo(({ childre
         ref={ref}
         animate={controls}
         initial="hidden"
-        transition={{ duration: 0.6 }} // changed duration to 0.6 for slower animation
+        transition={{ duration: 0.3 }}
         variants={{
-          visible: { opacity: 1, scale: 1, y: 0 }, // added y: 0 so that it ends at the original position
-          hidden: { opacity: 0, scale: 0, y: 100 } // added y: 100 to start from '100px' below
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 }
         }}
       >
         {children}
       </motion.div>
     </AnimatePresence>
   );
-});
-
+}
 export default FadeInWhenVisible;

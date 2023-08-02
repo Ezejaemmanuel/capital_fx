a375b5a080a31f2e1f523c453e061e99a5b3c69b
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-02 15:02:40 -0700

    still optimizing framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from '@/components/Header_AI';
 import SubSection from '@/components/SubSection';
 import data from '@/lib/subSectionData'
 import FadeInWhenVisible from './card-wrapper';
-import bouncingBitcoin from  '../public/bouncing-bitcoin.json'
+import bouncingBitcoin from '../public/bouncing-bitcoin.json'
 
 interface CryptoTraderAd {
   shortName: string;
@@ -16,26 +16,25 @@ interface CryptoTraderAd {
 const MyComponent: React.FC = () => {
   return (
     <>
-    <div className='dark:bg-dark-900'>
-      <PageWrapper >
+      <div className='dark:bg-dark-900'>
+
         <HeroSection />
         <div className="flex flex-col items-center justify-center">
           {data.map((item: CryptoTraderAd, index: number) => (
             <FadeInWhenVisible key={item.shortName}>
               <div className="w-full ">
-                <SubSection 
-                  imageURL={'https://www.arshakir.com/uploads/projects/saas-landing-page-freebie-4.webp'} 
-                  heading={item.heading} 
-                  subText={item.subText} 
-                  animationData={bouncingBitcoin} 
-                  reverseSection={index % 2 === 0} 
+                <SubSection
+                  imageURL={'https://www.arshakir.com/uploads/projects/saas-landing-page-freebie-4.webp'}
+                  heading={item.heading}
+                  subText={item.subText}
+                  animationData={bouncingBitcoin}
+                  reverseSection={index % 2 === 0}
                 />
               </div>
             </FadeInWhenVisible>
           ))}
         </div>
-      </PageWrapper>
-    </div>
+      </div>
     </>
   );
 };
